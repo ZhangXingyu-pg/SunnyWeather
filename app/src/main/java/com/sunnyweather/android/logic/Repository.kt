f2400a536c4b9884bb718044ab89cd0683b6f513
ZXY <809376255@qq.com>
2023-03-23 22:33:21 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,25 @@
+package com.sunnyweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+
+object Repository {
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok") {
+                val places = placeResponse.places
+                Result.success(places)
+            } else {
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        } catch (e: Exception) {
+            Result.failure<List<Place>>(e)
+        }
+
+        // 将包装的结果发射出去，该方法类似于调用LiveData的setValue()方法，来通知数据变化
+        emit(result)
+    }
+}
